14e3326393c12db38957c113b077a9beb237cb20
Beneba <afetsib@gmail.com>
2021-07-09 22:51:28 +0000

    myuser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import {Container, Row, Col } from 'react-bootstrap';
+import Users from './components/Users';
+import AddUserForm from './components/AddUserForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      users: [
+        {
+          name: "Bless Afetsi",
+          email: "bless@email.com",
+          gen : 2
+        }, 
+        {
+          name: "Ahmed zaky",
+          email: "zak@email.com",
+          gen : 13
+        },
+    
+        {
+          name: "Awal Mubarack",
+          email: "awal@email.com",
+          gen : 2
+        }
+      ]
+    }
+  }
+
+  addNewUser =(user) =>{
+    this.setState({
+      users: [...this.state.users, user ]
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Container fluid style = {{marginTop:"2rem"}} >
+          <Row>
+            <Col md="4" >
+              <AddUserForm  addUser ={this.addNewUser}/>
+            </Col>
+              
+            <Col>
+              <Users  usersData = {this.state.users} />
+            </Col>
+          </Row>
+  
+        </Container>
+        
+      </>
+    );
+  
+  }
 }
 
 export default App;
---
+++ src/components/AddUserForm.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+
+class AddUserForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      gen: "",
+    };
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addUser(this.state);
+    this.setState({
+      name: "",
+      email: "",
+      gen: "",
+    });
+    // console.log('submitted' ,this.state);
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Text>
+          <h1>Add Details Here </h1>
+        </Form.Text>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Name </Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            name="name"
+            onChange={this.handleChange}
+            value ={this.state.name}
+          />
+        </Form.Group>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              onChange={this.handleChange}
+              value ={this.state.email}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicGen">
+            <Form.Label>Gen</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Gen"
+              name="gen"
+              onChange={this.handleChange}
+              value ={this.state.gen}
+            />
+          </Form.Group>
+        </Form>
+
+        <Button variant="primary" type="submit" onSubmit={this.handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    );
+  }
+}
+export default AddUserForm;
